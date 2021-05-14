e5ca68a754f678297a9dc404b3d2bbb1514846f2
robherron <rlherron100@gmail.com>
2021-05-14 07:55:02 -0400

    Attempting to combine stripe arrays

--- pages/[name]/[id].js
+++ pages/[name]/[id].js
@@ -1,15 +1,32 @@
-import { data } from 'autoprefixer';
-import PropTypes, { number } from 'prop-types';
-import { useRouter } from 'next/router';
-import stripeClient from '../../client';
+import { data } from "autoprefixer";
+import PropTypes, { number } from "prop-types";
+import { useRouter } from "next/router";
+import stripeClient from "../../client";
 
 export async function getStaticProps() {
   const { data: products } = await stripeClient.products.list({
     active: true,
   });
+  const { data: prices } = await stripeClient.prices.list({
+    active: true,
+  });
+
+  const productsWithPrices = products.map((prod) => ({
+    id: prod.id,
+    images: prod.images,
+    description: prod.description,
+    name: prod.name,
+    price: "",
+  }));
+  productsWithPrices.forEach((prod) => {
+prod.price = 20;
+  });
+
   return {
     props: {
       products,
+      prices,
+      productsWithPrices,
     },
   };
 }
@@ -18,42 +35,64 @@ export async function getStaticPaths() {
   const { data: products } = await stripeClient.products.list({
     active: true,
   });
-  const paths = products.map((product) => ({
+  const { data: prices } = await stripeClient.prices.list({
+    active: true,
+  });
+  const productsWithPrices = products.map((prod) => ({
+    id: prod.id,
+    images: prod.images,
+    description: prod.description,
+    name: prod.name,
+    price: 20,
+  }));
+
+  const paths = productsWithPrices.map((product) => ({
     params: {
       id: product.id,
-      name: product.name
-    }
+      name: product.name,
+    },
   }));
 
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 }
 
 const propTypes = {
   products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  productsWithPrices: PropTypes.arrayOf(PropTypes.object).isRequired,
+  prices: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-const ProductItem = ({ products }) => {
+const ProductItem = ({ productsWithPrices, products }) => {
+  console.log("withprices", productsWithPrices, "products", products);
   // eslint-disable-next-line react/prop-types
-  const numberOfProducts = products.length;
+  const numberOfProducts = productsWithPrices.length;
   const router = useRouter();
-  const productArray = products.map((x) => x);
+  const productArray = productsWithPrices.map((x) => x);
   const { name, id } = router.query; // Destructuring our router object
   // eslint-disable-next-line no-console
-  console.log('Product Array: ', productArray);
+  console.log("Product Array: ", productArray);
   // eslint-disable-next-line no-console
-  console.log('route id', id);
+  console.log('route id', 'prod id', productArray.id);
   // eslint-disable-next-line no-console
   console.log(numberOfProducts);
   // eslint-disable-next-line no-console
   console.log(productArray.length);
 
+  productArray.forEach((prod) => console.log("shom", prod.id));
+
   const activeProd = productArray.filter((prod) => prod.id === id);
+  // {
+  //   for (let i = 0; i < prod.length; i + 1)
+  //   { if (prod[i].id === id) return prod[i].id; }
+  // });
+  console.log(activeProd);
 
   return (
     <div>
+      {console.log("active", activeProd)}
       <img className="w-75 " src={activeProd[0].images} alt="" />
       <h1 className="text-center font-bold text-xl mb-2">
         {activeProd[0].name}
@@ -65,7 +104,7 @@ const ProductItem = ({ products }) => {
 
       <h2>
         <b>Price:</b>
-        {activeProd[0].data}
+        {` $${activeProd[0].price}`}
       </h2>
     </div>
   );
@@ -73,47 +112,3 @@ const ProductItem = ({ products }) => {
 
 ProductItem.propTypes = propTypes;
 export default ProductItem;
-
-// import React from "react";
-// import PropTypes from "prop-types";
-// import stripeClient from "../../client";
-
-// export async function getStaticProps() {
-//   const { data: products } = await stripeClient.products.list({
-//     active: true,
-//   });
-
-//   return {
-//     props: {
-//       products,
-//     },
-//   };
-// }
-
-// const propTypes = {
-//   products: PropTypes.arrayOf(PropTypes.object).isRequired,
-// };
-
-// const Product = ({ products }) => {
-//   // eslint-disable-next-line no-console
-//   console.log(products);
-//   return (
-//     <ul>
-//       Product
-//       <li>
-//         {products.map((obj) => {
-//           // eslint-disable-next-line no-console
-//           console.log(obj);
-//           return (
-//             <ul>
-//               <li>{obj.name}</li>
-//               <li>{obj.description}</li>
-//             </ul>
-//           );
-//         })}
-//       </li>
-//     </ul>
-//   );
-// };
-// Product.propTypes = propTypes;
-// export default Product;
--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,39 @@ export async function getStaticProps() {
   const { data: prices } = await stripeClient.prices.list({
     active: true,
   });
-    // we want the unit_amount and .product
-    // isolate unit_amount from prices array, splice it into the product array
-  // prices.filter(product => product.id === "such and such")
+  // we want the unit_amount and .product
+  // isolate unit_amount from prices array, splice it into the product array
   const productsWithPrices = products.map((prod) => ({
     id: prod.id,
     images: prod.images,
     description: prod.description,
     name: prod.name,
-    price: 'Ill fix this later'
-  }
-  ));
+    price: '',
+  }));
+
+  productsWithPrices.forEach((element, i) => {
+
+    // 1. this iterates over each element once
+    // 2. for each element, compare its id value to every
+    // product value within the prices array looking for a match
+    // 3. if matched, take the unit_amount from the matching price object
+    // and ascribe that value to the price item in the active product element
+    // if (element[i].id === prices[i].products) {
+    // eslint-disable-next-line no-console
+    // const matchingPrice = prices.includes(element.id);
+    // console.log(element.id);
+    if (element.id !== prices[i].product) {
+      //   // element.price = prices[i].unit_amount;
+      console.log(prices[i].product, element.id);
+    }
+    // }
+  });
+
   return {
     props: {
       productsWithPrices,
-      products
+      products,
+      prices
     },
   };
 }
@@ -41,8 +59,8 @@ const Home = ({ products, prices, productsWithPrices }) => {
   // console.log('prod', products);
   // // eslint-disable-next-line no-console
   // console.log('Price', prices);
-  // eslint-disable-next-line no-console
-  console.log(productsWithPrices);
+  // // eslint-disable-next-line no-console
+  // console.log(productsWithPrices);
   return (
     <div className="container mx-auto w-full">
       <Head>
@@ -61,8 +79,11 @@ const Home = ({ products, prices, productsWithPrices }) => {
           {numberOfProducts}
         </span>
         <ul className="items-bottom min-h-full p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 ">
-          {products.map((obj) => (
-            <li key={obj.id} className=" p-4 max-w-sm rounded overflow-hidden shadow-lg border-t">
+          {productsWithPrices.map((obj) => (
+            <li
+              key={obj.id}
+              className=" p-4 max-w-sm rounded overflow-hidden shadow-lg border-t"
+            >
               <Link href={`/${obj.name}/${obj.id}`}>
                 <a>
                   <img className="w-full " src={obj.images} alt="" />
@@ -76,7 +97,7 @@ const Home = ({ products, prices, productsWithPrices }) => {
 
                   <h2>
                     <b>Price:</b>
-                    {obj.data}
+                    {` $${obj.price}`}
                   </h2>
                 </a>
               </Link>
